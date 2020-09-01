fd98199be729372c6f5816bdf6e57dbe8336d0a3
Anastasia Churikova <sattarova-91@inbox.ru>
2020-09-01 20:47:56 +0100

    add js

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const popup = document.querySelector('.popup')
+const popupOpenButton = document.querySelector('.profile__edit-button')
+const popupCloseButton = popup.querySelector('.popup__close')
+
+
+function popupToggle () {
+  popup.classList.toggle('popup_is-opened')
+}
+
+popupOpenButton.addEventListener('click', popupToggle)
+popupCloseButton.addEventListener('click', popupToggle)
+
+const profileName = document.querySelector('.profile__info_name')
+const profileJob = document.querySelector('.profile__info_job')
+const popupJob = document.querySelector('.popup__field[name="job"]')
+const popupName = document.querySelector('.popup__field[name="name"]')
+
+
+function fillPopupFields () {
+  popupJob.value = profileJob.textContent
+  popupName.value = profileName.textContent
+}
+
+popupOpenButton.addEventListener('click', fillPopupFields)
+
+const submitButton = document.querySelector('.popup__save-button')
+console.log (submitButton)
+
+function submitPopup (evt) {
+  evt.preventDefault();
+  alert('asd')
+  profileJob.textContent = popupJob.value
+  profileName.textContent = popupName.value
+}
+
+submitButton.addEventListener('submit', submitPopup)
+submitButton.addEventListener('click', popupToggle)
