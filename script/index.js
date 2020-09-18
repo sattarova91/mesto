3f7c382e25ba56283d1dc2e09dd40188dc10e272
Anastasia Churikova <sattarova-91@inbox.ru>
2020-09-18 23:15:14 +0100

    add js cards

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+//POPUP EDIT
 
 const popupEdit = document.querySelector('.popup-edit')
 const popupEditOpenButton = document.querySelector('.profile__edit-button')
@@ -17,12 +18,12 @@ const popupJob = popupEdit.querySelector('.popup__field[name="job"]')
 const popupName = popupEdit.querySelector('.popup__field[name="name"]')
 
 
-function fillPopupFields () {
+function fillPopupEditFields () {
   popupJob.value = profileJob.textContent
   popupName.value = profileName.textContent
 }
 
-popupEditOpenButton.addEventListener('click', fillPopupFields)
+popupEditOpenButton.addEventListener('click', fillPopupEditFields)
 
 const submitEditButton = popupEdit.querySelector('.popup-edit__save-button')
 
@@ -35,11 +36,104 @@ function submitEditPopup (evt) {
 submitEditButton.addEventListener('click', submitEditPopup)
 submitEditButton.addEventListener('click', () => {popupToggle(popupEdit)})
 
-const popupAdd = document.querySelector('.popup-add')
-const popupAddOpenButton = document.querySelector('.profile__add-button')
-const popupAddCloseButton = popupAdd.querySelector('.popup__close')
+//CARDS UI
+
+// LIKE CARD
+
+function likeToggle (likeButton) {
+  likeButton.classList.toggle('elements__like-button_liked')
+}
 
 
-popupAddOpenButton.addEventListener('click', () => {popupToggle(popupAdd)})
-popupAddCloseButton.addEventListener('click', () => {popupToggle(popupAdd)})
+let cards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
+
+const cardTemplate = document.querySelector('#cards').content;
+const cardsContainer = document.querySelector('.elements');
+
+function renderCard(card)  {
+  let cardHTML = cardTemplate.cloneNode(true);
+  cardHTML.querySelector('.elements__title').textContent = card.name;
+  cardHTML.querySelector('.elements__img').src = card.link;
+  cardHTML.querySelector('.elements__img').alt = card.name;
+  cardsContainer.prepend(cardHTML);
+  let likeButton = cardsContainer.querySelector('.elements__like-button');
+  likeButton.addEventListener('click', () => {likeToggle(likeButton)});
+
+}
+
+function renderCards() {
+  cards.reverse().forEach(renderCard);
+}
+
+function addCard(card) {
+  if (!card.name || !card.link) {
+    alert('Заполните поля "название" и "ссылка" у карточки');
+  } else {
+    cards.unshift(card);
+    renderCard(card);
+  }
+}
+
+//POPUP ADD CARD
+
+const popupAdd = document.querySelector('.popup-add');
+const popupAddOpenButton = document.querySelector('.profile__add-button');
+const popupAddCloseButton = popupAdd.querySelector('.popup__close');
+const popupAddCreateButton = popupAdd.querySelector('.popup-add__add-button');
+const popupAddTitle = popupAdd.querySelector('.popup-add__field[name="title"]');
+const popupAddLink = popupAdd.querySelector('.popup-add__field[name="link"]');
+
+function popudAddCleanFields() {
+  popupAddLink.value = '';
+  popupAddTitle.value = '';
+}
+
+popupAddOpenButton.addEventListener('click', () => {popupToggle(popupAdd)});
+popupAddCloseButton.addEventListener('click', () => {
+  popupToggle(popupAdd);
+  popudAddCleanFields();
+});
+
+popupAddCreateButton.addEventListener('click', () => {popupToggle(popupAdd)});
+popupAddCreateButton.addEventListener('click', () => {
+  addCard( {
+    name: popupAddTitle.value,
+    link: popupAddLink.value
+  })
+  popudAddCleanFields();
+});
+
+popupAddCreateButton.addEventListener('click', () => {
+  popupAddLink.value = '';
+  popupAddTitle.value = '';
+});
+
+
+
+renderCards();
