cb0da200e9d206ac6a601041b0da9b5625826d1d
Anastasia Churikova <sattarova-91@inbox.ru>
2021-02-27 20:24:15 +0000

    Updates

--- src/scripts/components/EditPopup.js
+++
@@ -1,9 +0,0 @@
-import PopupWithForm from './PopupWithForm.js';
-
-export default class EditPopup extends PopupWithForm {
-  open({name, info}) {
-    this._element.querySelector('#name-field').value = name;
-    this._element.querySelector('#job-field').value = info;
-    super.open();
-  }
-}
